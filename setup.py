a8f5a1d6d30935c2f521eff210ec394c9cf7f2a6
mitsakos178 <76257091+mitsakos178@users.noreply.github.com>
2022-09-20 19:22:24 +0300

    changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='QoI-AI-Consensus Protocol',
+    version='1.0',
+    packages=['Utils'],
+    url='-',
+    license='-',
+    author='Dimitrios Papaioannou',
+    author_email='dpapaion@webmail.auth.gr',
+    description='Distributed DNN Inference Consensus Protocol'
+)
